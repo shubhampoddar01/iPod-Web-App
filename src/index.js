51da2e17bbcc7a8bbb4a711bed3b3c01cd1685c4
shubhampoddar01 <shubham.poddar0105@gmail.com>
2023-03-26 03:04:09 +0530

    Completing Ipod Web App

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 import React from "react";
-import ReactDOM from "react-dom";
+import ReactDOM from "react-dom/client";
 import "./assets/css/index.css";
 import App from "./components/stateful/App";
 
