c7f2550dfa2258d29eddeb0f94164f753775ecc8
shubhampoddar01 <shubham.poddar0105@gmail.com>
2023-03-26 02:57:55 +0530

    Completing Ipod Web App

--- src/index.js
+++ src/index.js
@@ -4,9 +4,16 @@ import "./assets/css/index.css";
 import App from "./components/stateful/App";
 
 // Rendering the React App into the HTML DOM Element "root"
-ReactDOM.render(
-	<React.StrictMode>
-		<App />
-	</React.StrictMode>,
-	document.getElementById("root")
+// ReactDOM.render(
+// 	<React.StrictMode>
+// 		<App />
+// 	</React.StrictMode>,
+// 	document.getElementById("root")
+// );
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
 );
